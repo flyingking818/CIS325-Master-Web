61f6d89d939e61c1b5e3d3785b234f86e6fda5d4
Jeremy Wang <JWang@flagler.edu>
2025-09-08 20:57:50 -0400

    Add project files.

---
+++ Pages/Demos/Module1/Welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIS325_Master_Web.Pages.Demos.Module1
+{
+    public class WelcomeModel : PageModel  //WelcomeModel is child class of the PageModel parent class
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
