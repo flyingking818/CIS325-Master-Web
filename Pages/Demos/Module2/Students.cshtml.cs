6e45ea119562aa2c298b5af5d7778951944a24ed
Jeremy Wang <JWang@flagler.edu>
2025-09-15 20:45:26 -0400

    9-15-2025

--- Pages/Demos/Module2/Students.cshtml.cs
+++ Pages/Demos/Module2/Students.cshtml.cs
@@ -23,6 +23,8 @@ namespace CIS325_Master_Web.Pages.Demos.Module2
         }
 
         //Create an in-memory array for the student list (hard-coded)
+        //Let's use the Contains() method to search for a substring
+        //which is equivalent to SQL's LIKE '%query%'
         private Student[] Students =
         {
             new("S101", "Jeremy Wang"),
@@ -37,10 +39,28 @@ namespace CIS325_Master_Web.Pages.Demos.Module2
 
         //Handler method for HTTP GET requests
         //? is an optional, because we don't know whether there's a query!
-        public void OnGet(string? query)
+        public void OnGet(string? query)  //This MUST match the name of the query string parameter
         {
             //This is where we need to do the heavy-lifting! Logic/algorithm, etc.
             Query = query;
+            if (string.IsNullOrEmpty(query)){
+                //Show all students
+                Results = Students.ToList();
+            }
+            else
+            {
+                //Refresh - SQL = Structured Query Language
+                //LINQ = Language Integrated Query, SQL like syntax in C#, which can be used 
+                //to query in-memory collections (e.g., arrays, lists, collections,
+                //databases, xml, JSON, etc.)
+                Results = Students.Where(
+                        s=>s.Id.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+                        s.Name.Contains(query, StringComparison.OrdinalIgnoreCase)
+                        )
+                        .ToList();
+
+            }
+
         }
     }
 }
