52408d3a85a221e0923c35b0c8c59bddb7e22dac
Jeremy Wang <JWang@flagler.edu>
2025-09-22 18:00:37 -0400

    9-22

---
+++ Pages/Demos/Module3/FlaglerCostCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIS325_Master_Web.Pages.Demos.Module3
+{
+    public class FlaglerCostCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
